5930c532c94f21df7cfbc0ccf427c4ed7a976098
al <al@el-machine.com>
2024-05-02 19:21:05 +0700

    LICENSE-2.0 https://apache.org/licenses/LICENSE-2.0

--- Sources/Wand/Wand_Expressible.swift
+++ Sources/Wand/Wand_Expressible.swift
@@ -1,9 +1,20 @@
-//
-//  Wand_Expressible.swift
-//  Wand
-//
-//  Created by al on 15.04.2024.
-//
+///
+/// Copyright © 2020-2024 El Machine 🤖
+/// https://el-machine.com/
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+/// 1) LICENSE file
+/// 2) https://apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
 
 import Foundation
 
