e702bb6a599edd934ead16a98a86e2c97c68c12b
al <al@el-machine.com>
2024-05-02 19:38:23 +0700

    Add credits

--- Tests/Wand/Wand_Tests.swift
+++ Tests/Wand/Wand_Tests.swift
@@ -15,6 +15,8 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
+/// Created by Alex Kozin
+/// 2020 El Machine
 
 import CoreLocation.CLLocation
 
