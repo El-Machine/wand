2932277786f7143d483de219e35cae2171aa9d2e
al <al@el-machine.com>
2024-05-17 19:12:05 +0900

    Rename scheme

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "wand",
+    name: "Wand",
     defaultLocalization: "ru",
 
     platforms: [
@@ -34,7 +34,7 @@ let package = Package(
     ],
 
     products: [
-        .library(name: "wand", targets: ["wand"])
+        .library(name: "Wand", targets: ["wand"])
     ],
 
     dependencies: [
